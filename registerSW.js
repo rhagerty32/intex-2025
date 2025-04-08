a60c36a300b3ad700c612162a9e37ecfb18d273f
rhagerty32 <ry2402@gmail.com>
2025-04-08 16:20:28 -0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/intex-2025/sw.js', { scope: '/intex-2025/' })})}
